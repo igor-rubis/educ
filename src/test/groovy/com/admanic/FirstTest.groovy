0b6923d2132049af12486bf7c889fdcfedb19186
sergeykizim <serger.kizim.87@gmail.com>
2018-02-21 17:40:38 +0200

    First commit

---
+++ src/test/groovy/com/admanic/FirstTest.groovy
@@ -0,0 +1,41 @@
+package com.admanic
+
+import io.github.bonigarcia.wdm.FirefoxDriverManager
+import org.junit.Test
+import org.openqa.selenium.By
+import org.openqa.selenium.WebDriver
+import org.openqa.selenium.WebElement
+import org.openqa.selenium.firefox.FirefoxDriver
+import org.openqa.selenium.support.ui.ExpectedCondition
+import org.openqa.selenium.support.ui.WebDriverWait
+
+class FirstTest {
+    @Test
+    void firstTest1() {
+        FirefoxDriverManager.getInstance().setup()
+        WebDriver driver = new FirefoxDriver()
+
+        driver.get("http://www.google.com")
+
+        WebElement element = driver.findElement(By.name("q"))
+
+        element.sendKeys("Cheese!")
+
+
+        element.submit()
+
+
+        System.out.println("Page title is: " + driver.getTitle())
+
+
+        (new WebDriverWait(driver, 10)).until(new ExpectedCondition<Boolean>() {
+            Boolean apply(WebDriver d) {
+                return d.getTitle().toLowerCase().startsWith("cheese!")
+            }
+        })
+
+        System.out.println("Page title is: " + driver.getTitle())
+
+        driver.quit()
+    }
+}
