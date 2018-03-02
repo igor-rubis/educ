6fe877b810ea69e13a8afd18da28ad000191504c
chesnuk <m.chesnuyk@gmail.com>
2018-03-02 18:35:09 +0200

    fix

--- src/test/groovy/com/admanic/misha/FirstTest.groovy
+++ src/test/groovy/com/admanic/misha/FirstTest.groovy
@@ -1,6 +1,7 @@
-package com.admanic
+package com.admanic.misha
 
 import io.github.bonigarcia.wdm.FirefoxDriverManager
+import org.junit.After
 import org.junit.Test
 import org.openqa.selenium.By
 import org.openqa.selenium.WebDriver
@@ -9,24 +10,27 @@ import org.openqa.selenium.firefox.FirefoxDriver
 import org.openqa.selenium.support.ui.ExpectedCondition
 import org.openqa.selenium.support.ui.WebDriverWait
 
+import static org.hamcrest.MatcherAssert.assertThat
+import static org.hamcrest.core.IsEqual.equalTo
+
 class FirstTest {
+    WebDriver driver
+
     @Test
     void firstTest1() {
         FirefoxDriverManager.getInstance().setup()
-        WebDriver driver = new FirefoxDriver()
+        driver = new FirefoxDriver()
 
         driver.get("http://www.google.com")
 
         WebElement element = driver.findElement(By.name("q"))
 
+        element.clear()
         element.sendKeys("Cheese!")
-
 
         element.submit()
 
-
         System.out.println("Page title is: " + driver.getTitle())
-
 
         (new WebDriverWait(driver, 10)).until(new ExpectedCondition<Boolean>() {
             Boolean apply(WebDriver d) {
@@ -36,6 +40,11 @@ class FirstTest {
 
         System.out.println("Page title is: " + driver.getTitle())
 
+        assertThat(driver.getTitle().to, equalTo('Cheese! - Пошук Google'))
+    }
+
+    @After
+    void tearDown() {
         driver.quit()
     }
 }
