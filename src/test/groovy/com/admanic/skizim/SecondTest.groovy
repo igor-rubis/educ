cbbbe12f05b783f4378046a2be3e83515b80991f
igor-rubis <rubis.igor@gmail.com>
2018-03-14 18:46:11 +0200

    combined browser classes into single class

--- src/test/groovy/com/admanic/skizim/SecondTest.groovy
+++ src/test/groovy/com/admanic/skizim/SecondTest.groovy
@@ -4,24 +4,24 @@ import org.junit.After
 import org.junit.Test
 import org.openqa.selenium.By
 import org.openqa.selenium.WebElement
+import com.admanic.commons.browser.Browser
 
-import static com.admanic.skizim.Browser.*
 import static org.hamcrest.CoreMatchers.containsString
 import static org.hamcrest.MatcherAssert.assertThat
 
 class SecondTest {
     @Test
     void secondTest2() {
-        getInstance().get("https://pn.com.ua/")
+        Browser.getInstance().get("https://pn.com.ua/")
 
-        WebElement laptops = getInstance().findElement(By.cssSelector('[href*="1003"]'))
+        WebElement laptops = Browser.getInstance().findElement(By.cssSelector('[href*="1003"]'))
         laptops.click()
 
-        WebElement firstItem = getInstance().findElement(By.cssSelector(".catalog-block-head>a"))
+        WebElement firstItem = Browser.getInstance().findElement(By.cssSelector(".catalog-block-head>a"))
         String fItem = firstItem.getText()
         firstItem.click()
 
-        WebElement itemPage = getInstance().findElement(By.cssSelector(".breadcrumb>li>h1>span"))
+        WebElement itemPage = Browser.getInstance().findElement(By.cssSelector(".breadcrumb>li>h1>span"))
         String iPage = itemPage.getText()
 
         assertThat(iPage, containsString(fItem))
@@ -30,6 +30,6 @@ class SecondTest {
 
     @After
     void tearDown() {
-        killBrowser()
+        Browser.killBrowser()
     }
 }
