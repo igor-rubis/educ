fdf81382e5e9e26340714bf31f91ead4496692ca
chesnuk <m.chesnuyk@gmail.com>
2018-03-15 09:27:28 +0200

    change getInstance() to instance

--- src/test/groovy/com/admanic/misha/FirstTest.groovy
+++ src/test/groovy/com/admanic/misha/FirstTest.groovy
@@ -17,46 +17,46 @@ class FirstTest {
 
     @Test
     void firstTest1() {
-        Browser.getInstance().get("http://www.google.com")
+        Browser.instance.get("http://www.google.com")
 
-        WebElement element = Browser.getInstance().findElement(By.name("q"))
+        WebElement element = Browser.instance.findElement(By.name("q"))
 
         element.clear()
         element.sendKeys("Cheese!")
 
         element.submit()
 
-        System.out.println("Page title is: " + Browser.getInstance().getTitle())
+        System.out.println("Page title is: " + Browser.instance.getTitle())
 
-        (new WebDriverWait(Browser.getInstance(), 10)).until(new ExpectedCondition<Boolean>() {
+        (new WebDriverWait(Browser.instance, 10)).until(new ExpectedCondition<Boolean>() {
             Boolean apply(WebDriver d) {
                 return d.getTitle().toLowerCase().startsWith("cheese!")
             }
         })
 
-        System.out.println("Page title is: " + Browser.getInstance().getTitle())
+        System.out.println("Page title is: " + Browser.instance.getTitle())
 
-        assertThat(Browser.getInstance().getTitle(), equalTo('Cheese! - Пошук Google'))
+        assertThat(Browser.instance.getTitle(), equalTo('Cheese! - Пошук Google'))
     }
 
     @Test
     void firstTest2() {
 
-        Browser.getInstance().get("https://hotline.ua/")
+        Browser.instance.get("https://hotline.ua/")
 
-        def cityPopUpCloseButton = Browser.getInstance().findElement(By.cssSelector("[data-dropdown-id='confirm-city-detect'] .close"))
+        def cityPopUpCloseButton = Browser.instance.findElement(By.cssSelector("[data-dropdown-id='confirm-city-detect'] .close"))
         cityPopUpCloseButton.click()
 
-        def element = Browser.getInstance().findElement(By.name("q"))
+        def element = Browser.instance.findElement(By.name("q"))
 
         element.clear()
         element.sendKeys("iphone x")
 
         element.submit()
 
-        new WebDriverWait(Browser.getInstance(), 10).until(ExpectedConditions.presenceOfElementLocated(By.cssSelector('div.pages-list [data-page="2"]')))
+        new WebDriverWait(Browser.instance, 10).until(ExpectedConditions.presenceOfElementLocated(By.cssSelector('div.pages-list [data-page="2"]')))
 
-        def element1 = Browser.getInstance().findElement(By.cssSelector('div.pages-list [data-page="2"]'))
+        def element1 = Browser.instance.findElement(By.cssSelector('div.pages-list [data-page="2"]'))
         element1.click()
 
     }
