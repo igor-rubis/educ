9612de3ee505c73e3f95d67a6e39548d04ced7c8
sergeykizim <serger.kizim.87@gmail.com>
2018-03-27 18:21:47 +0300

    SecondTest class refactoring

--- src/test/groovy/com/admanic/skizim/SecondTest.groovy
+++ src/test/groovy/com/admanic/skizim/SecondTest.groovy
@@ -3,8 +3,6 @@ package com.admanic.skizim
 import com.admanic.commons.browser.Browser
 import org.junit.After
 import org.junit.Test
-import org.openqa.selenium.By
-import org.openqa.selenium.WebElement
 
 import static org.hamcrest.CoreMatchers.containsString
 import static org.hamcrest.MatcherAssert.assertThat
@@ -12,22 +10,14 @@ import static org.hamcrest.MatcherAssert.assertThat
 class SecondTest {
     @Test
     void secondTest2() {
-        Browser.instance.get("https://pn.com.ua/")
-
-        WebElement laptops = Browser.instance.findElement(By.cssSelector('[href*="1003"]'))
-        laptops.click()
-
-        WebElement firstItem = Browser.instance.findElement(By.cssSelector(".catalog-block-head>a"))
-        String fItem = firstItem.getText()
-        firstItem.click()
-
-        WebElement itemPage = Browser.instance.findElement(By.cssSelector(".breadcrumb>li>h1>span"))
-        String iPage = itemPage.getText()
-
-        assertThat(iPage, containsString(fItem))
-
+        Browser.instance.get('https://pn.com.ua/')
+        HomePage home = new HomePage()
+        home.openLaptops()
+        LaptopsPage laptops = new LaptopsPage()
+        laptops.openFirstItem()
+        ProductPage product = new ProductPage()
+        assertThat(product.getNameText(), containsString (laptops.getFirstItemText()))
     }
-
     @After
     void tearDown() {
         Browser.killBrowser()
